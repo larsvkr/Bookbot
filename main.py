d6ccd59e04aa9b0f6c5b46aab4dc9ec5f9cdbe8e
lars <larsvkr@outlook.com>
2025-04-10 10:35:53 +0200

    Extra function, gives the number of chars in a text.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from stats import word_count, get_book_text
+from stats import word_count, get_book_text, char_count
 def main():
     contents = get_book_text("books/frankenstein.txt")
     print(word_count(contents))
+    print(char_count(contents))
+
+
 
 main()
--- stats.py
+++ stats.py
@@ -7,3 +7,15 @@ def word_count(text):
     number_of_words = len(text.split())
     return f"{number_of_words} words found in the document"
 
+def char_count(text):
+    amount = {}
+    text = text.lower()
+    for char in text:
+        if char in amount.keys():
+            amount[char] += 1 
+        else:
+            amount[char] = 1
+    return amount
+        
+
+
