1633a2db5277cd01c043a6097a50c85d00733fdd
lars <larsvkr@outlook.com>
2025-04-10 10:19:17 +0200

    refactored main into stats.py and main

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-def get_book_text(url):
-    with open(url) as f:
-        read_contents = f.read()
-
-    return read_contents
-
-def word_count(text):
-    number_of_words = len(text.split())
-    return f"{number_of_words} words found in the document"
-
+from stats import word_count, get_book_text
 def main():
     contents = get_book_text("books/frankenstein.txt")
     print(word_count(contents))
